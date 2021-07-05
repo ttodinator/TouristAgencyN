e5448a8e3b50b9356affc058f4b78a08bc40a774
ttodinator <ttodinator@gmail.com>
2021-07-05 13:10:28 +0000

    Dodate DTO (Data Transfer Object klase) za prijem podataka za rad sa destinacijama

---
+++ API/DTOs/DestinationDto.cs
@@ -0,0 +1,26 @@
+﻿using API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTOs
+{
+    public class DestinationDto
+    {
+        public int Id { get; set; }
+        public string City { get; set; }
+        public string Hotel { get; set; }
+        public string Transportation { get; set; }
+        public int Stars { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public DateTime DateAdded { get; set; } = DateTime.Now;
+        public string Type { get; set; }
+        bool isLikedByCurrentUser { get; set; } = false;
+        public List<DestinationRooms> Rooms { get; set; }
+        public List<Photo> Photos { get; set; }
+        public string PhotoUrl { get; set; }
+        public int LikesCount { get; set; } = 0;
+    }
+}
---
+++ API/DTOs/UpdateDestinationSto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTOs
+{
+    public class UpdateDestinationDto
+    {
+        public int Id { get; set; }
+        public string City { get; set; }
+        public string Hotel { get; set; }
+        public string Transportation { get; set; }
+        public int Stars { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public DateTime DateAdded { get; set; } = DateTime.Now;
+        public string Type { get; set; }
+
+        public List<int> Rooms { get; set; }
+    }
+}
