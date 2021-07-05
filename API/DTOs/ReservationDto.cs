4532e5757031bb8f67336f43a1432dae96d4216b
ttodinator <ttodinator@gmail.com>
2021-07-05 13:25:57 +0000

    Dodate DTO klase zza rad sa rezervacijama

---
+++ API/DTOs/ReservationDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTOs
+{
+    public class ReservationDto
+    {
+        public int DestinationId { get; set; }
+        public List<DateTime> Dates { get; set; }
+        public int NumberOfPeople { get; set; }
+        public double TotalPrice { get; set; }
+    }
+}
---
+++ API/DTOs/ReservedDestinationsDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTOs
+{
+    public class ReservedDestinationsDto
+    {
+        public int DestinationId { get; set; }
+        public string DestinationCity { get; set; }
+        public string DestinationHotel { get; set; }
+        public int NumberOfPeople { get; set; }
+        public double TotalPrice { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
