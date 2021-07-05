620d977bb7e06636af66186c4fd058cd82d16508
ttodinator <ttodinator@gmail.com>
2021-07-05 13:18:56 +0000

    Dodate DTO klase za izmenu korisnika i dodavanje destinacije

---
+++ API/DTOs/UserEditDto.cs
@@ -0,0 +1,28 @@
+﻿using API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTOs
+{
+    public class UserDto
+    {
+        public string Username { get; set; }
+        public string Token { get; set; }
+        public List<Destination> Destination { get; set; }
+
+        public List<Destination> Resevations { get; set; }
+
+        public List<int> Likes { get; set; }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public string CellphoneNumber { get; set; }
+
+        public string UserEmail { get; set; }
+
+    }
+}
