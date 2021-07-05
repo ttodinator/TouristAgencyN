c63ff12fe40c71edd0cd9cd1eaf617472a31dfec
ttodinator <ttodinator@gmail.com>
2021-07-05 13:47:36 +0000

    Dodate DTO klase za rad sa fotografijama i lajkovima

---
+++ API/Entities/EditPhotoDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class EditPhotoDto
+    {
+        public int photoId { get; set; }
+        public int destinationId { get; set; }
+
+    }
+}
