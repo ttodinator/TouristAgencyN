3a1a2c2b93cfda2b7613c738e421b1bffc71a15a
ttodinator <ttodinator@gmail.com>
2021-07-05 13:55:52 +0000

    Dodate klase neophodne za paginaciju, sortiranje i filtriranje destinacija

---
+++ API/Helpers/DestinationParams.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Helpers
+{
+    public class DestinationParams : PaginationParams
+    {
+        public string CurrentDestination { get; set; }
+        public string Type { get; set; } = "all";
+        public double minPrice { get; set; } = 1;
+        public double maxPrice { get; set; } = 1000;
+        public string OrderBy { get; set; } = "dateAdded";
+    }
+}
