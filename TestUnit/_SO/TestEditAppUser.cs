8aa7994b9feb26e03d206acb1db07c941391ca11
ttodinator <ttodinator@gmail.com>
2021-07-08 12:09:24 +0000

    Dodata klasa za testiranje sistemske operacije izmene korisnika aplikacije

--- TestUnit/_SO/TestEditAppUser.cs
+++ TestUnit/_SO/TestEditAppUser.cs
@@ -10,32 +10,25 @@ using Microsoft.EntityFrameworkCore;
 namespace TestUnit._SO
 {
     [TestFixture]
-    class TestAppUserLikeSO
+    class TestEditAppUser
     {
         TestContext context;
         AppUser appUser;
-        Destination destination;
-        Like like;
 
         [SetUp]
         public void su()
         {
             context = new TestContext();
             appUser = new AppUser("name", "surname", DateTime.Now, "number", "email");
-            destination = new Destination(0, "city", "hotel", "trans", 5, "desc", 10, DateTime.Now,"type");
             context.AspNetUsers.Attach(appUser);
-            context.Destination.Attach(destination);
             context.AspNetUsers.Add(appUser);
-            context.Destination.Add(destination);
             context.SaveChanges();
         }
 
         [TearDown]
         public void td()
         {
-            context.Remove(like);
             context.Remove(appUser);
-            context.Remove(destination);
             context.SaveChanges();
             context.Dispose();
 
@@ -46,14 +39,10 @@ namespace TestUnit._SO
         public void TestAddLike()
         {
             AppUser appUser1 = context.AspNetUsers.FirstOrDefault(x => x.Name == "name");
-            Destination destination1 = context.Destination.FirstOrDefault(x => x.City == "city");
-            like = new Like(appUser1.Id, destination1.Id);
-            appUser1.Likes = new List<Like>();
-            appUser1.Likes.Add(like);
+            appUser1.Name = "AAA";
             context.SaveChanges();
-            AppUser appUser2 = context.AspNetUsers.Include(x => x.Likes).FirstOrDefault(x => x.Id == appUser1.Id);
-            Assert.AreEqual(appUser2, appUser);
+            AppUser appUser2 = context.AspNetUsers.FirstOrDefault(x => x.Name == "AAA");
+            Assert.AreEqual(appUser1, appUser2);
         }
-
     }
 }
