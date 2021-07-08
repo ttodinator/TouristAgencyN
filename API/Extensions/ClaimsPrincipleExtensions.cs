100602571efa494482b415157782209b21319075
ttodinator <ttodinator@gmail.com>
2021-07-08 15:16:28 +0000

    Dokumentovane helper klase

--- API/Extensions/ClaimsPrincipleExtensions.cs
+++ API/Extensions/ClaimsPrincipleExtensions.cs
@@ -6,12 +6,25 @@ using System.Threading.Tasks;
 
 namespace API.Extensions
 {
+    /// <summary>
+    /// Helper staticka koja klasa koja sluzi za dobijanje podataka o trenutnom korisniku
+    /// </summary>
     public static class ClaimsPrincipleExtensions
     {
+        /// <summary>
+        /// Staticka metoda koja pronalazi Name za trenutno ulogovanog korisnika
+        /// </summary>
+        /// <param name="user">Trenutno ulogovani korisnik</param>
+        /// <returns>Name korisnika</returns>
         public static string GetUsername(this ClaimsPrincipal user)
         {
             return user.FindFirst(ClaimTypes.Name)?.Value;
         }
+        /// <summary>
+        /// Staticka metoda koja pronalazi Id za trenutno ulogovanog korisnika
+        /// </summary>
+        /// <param name="user">Trenutno ulogovani korisnik</param>
+        /// <returns>Jedinstveni identifikator(Id) korisnika</returns
 
         public static int GetUserId(this ClaimsPrincipal user)
         {
--- API/Extensions/Httpextensions.cs
+++ API/Extensions/Httpextensions.cs
@@ -8,8 +8,19 @@ using System.Threading.Tasks;
 
 namespace API.Extensions
 {
+    /// <summary>
+    /// Staticka heleper klasa koja sluzi za rad sa http zahtevima
+    /// </summary>
     public static class HttpExtensions
     {
+        /// <summary>
+        /// Staticka metoda koja postavlja paginacioni header u zaglvalje http zahteva
+        /// </summary>
+        /// <param name="response">odnosi se na http zahtev</param>
+        /// <param name="currentPage">trenutna strana</param>
+        /// <param name="itemsPerPage">broj stavki po strani</param>
+        /// <param name="totalItems">ukupan broj stavki</param>
+        /// <param name="totalPages">ukupan brojj stranica</param>
         public static void AddPaginationHeader(this HttpResponse response, int currentPage, int itemsPerPage, int totalItems, int totalPages)
         {
             var paginationHeader = new PaginationHeader(
