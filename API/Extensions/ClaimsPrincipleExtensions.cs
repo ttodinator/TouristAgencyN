581cb88460b2d4b8ff8b7e675edd4725ef43ad50
ttodinator <ttodinator@gmail.com>
2021-07-08 14:04:50 +0000

    Izmenjena klasa ClaimsPrincipleExtensions

--- API/Extensions/ClaimsPrincipleExtensions.cs
+++ API/Extensions/ClaimsPrincipleExtensions.cs
@@ -15,7 +15,8 @@ namespace API.Extensions
 
         public static int GetUserId(this ClaimsPrincipal user)
         {
-            return int.Parse(user.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+            int a= int.Parse(user.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+            return a;
         }
     }
 }
